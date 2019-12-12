984d19bb801c988516b7c0a6aa450c2c30757c40
Jack Bates <jack@nottheoilrig.com>
2019-12-12 08:37:51 -0700

    xpipe(): enable dynamic number of arguments

--- src/rediff.c
+++ src/rediff.c
@@ -905,8 +905,8 @@ static int rediff (const char *original, const char *edited, FILE *out)
 	last->num_lines = linenum - last->line_in_diff + 1;
 
 	/* Run diff between original and edited. */
-	t = xpipe (DIFF, &child, "r", DIFF, "-U0",
-		   original, edited, NULL);
+	t = xpipe (DIFF, &child, "r", (char **) (const char *[]) { DIFF, "-U0",
+		   original, edited, NULL });
 	m = xtmpfile ();
 	if (m) {
 		size_t buffer_size = 10000;
