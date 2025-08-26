fcf9dc13080bb173551a382e2b2eac2f60bfb898
Tim Waugh <twaugh@redhat.com>
2025-08-26 21:36:45 +0100

    Get gnulib from github.com (faster, more reliable)
    Suggestion from @hhoffstaette

--- gnulib-update.sh
+++ gnulib-update.sh
@@ -3,7 +3,7 @@
 # Run this when you want to update to newer gnulib versions
 
 echo "Updating gnulib modules..."
-git clone --depth 1 https://git.savannah.gnu.org/git/gnulib.git /tmp/gnulib-update
+git clone --depth 1 https://github.com/coreutils/gnulib.git /tmp/gnulib-update
 /tmp/gnulib-update/gnulib-tool --update
 rm -rf /tmp/gnulib-update
 echo "Gnulib updated. Please test and commit."
